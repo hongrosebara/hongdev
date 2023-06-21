73e3bcf18e2e0a13d4c56a5b4bb0e431ed3888a6
Hong Le <hongrosebara@gmail.com>
2023-06-21 12:32:04 +0100

    changed experiences

--- components/SkillCard/SkillCard.jsx
+++ components/SkillCard/SkillCard.jsx
@@ -2,28 +2,28 @@ const SkillCard = () => {
   return (
     <section className="section-skills">
       <div className="skill__item">
-        <h2>Next.js</h2>
+        <h2>E-commerce</h2>
+        <span className="skill__item--experience">5 Years Experience</span>
+      </div>
+      <div className="skill__item">
+        <h2>Affiliate SEO</h2>
+        <span className="skill__item--experience">2 Year Experience</span>
+      </div>
+      <div className="skill__item">
+        <h2>Programmatic SEO</h2>
         <span className="skill__item--experience">1 Year Experience</span>
       </div>
       <div className="skill__item">
         <h2>Javascript</h2>
-        <span className="skill__item--experience">2 Years Experience</span>
-      </div>
-      <div className="skill__item">
-        <h2>Sass</h2>
-        <span className="skill__item--experience">2 Years Experience</span>
-      </div>
-      <div className="skill__item">
-        <h2>Vue.js</h2>
-        <span className="skill__item--experience">1 Year Experience</span>
+        <span className="skill__item--experience">3 Years Experience</span>
       </div>
       <div className="skill__item">
         <h2>Python</h2>
         <span className="skill__item--experience">1 Year Experience</span>
       </div>
       <div className="skill__item">
-        <h2>SEO</h2>
-        <span className="skill__item--experience">3 Years Experience</span>
+        <h2>Web Design</h2>
+        <span className="skill__item--experience">2 Years Experience</span>
       </div>
       
     </section>
